79ee2740ae72a046b0fc98af5f6a68fcc3015cc0
김진영 <luckyjin98@naver.com>
2020-05-19 12:56:33 +0000

    init

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-import passport from "passport";
+import passport from "passport"; 
 import routes from "../routes";
 import User from "../models/User";
 
@@ -36,13 +36,81 @@ export const postLogin = passport.authenticate("local", {
   successRedirect: routes.home
 });
 
+export const githubLogin = passport.authenticate("github");
+
+export const githubLoginCallback = async (_, __, profile, cb) => {
+	const { _json: {id, avatar_url: avatarUrl, name, email} } = profile;
+	console.log(profile);
+	try{
+		const user = await User.findOne({email});
+		if (user) {
+			user.githubId = id;
+			user.save();
+			return cb(null, user);
+		}
+		const newUser = await User.create({
+			email,
+			name,
+			githubId: id,
+			avatarUrl
+		});
+			return cb(null, newUser);
+	} catch(error) {
+		return cb(error);
+	}
+};
+
+export const postGithubLogin = (req, res) => {
+	res.redirect(routes.home);
+};
+
+export const facebookLogin = passport.authenticate('facebook');
+
+export const facebookLoginCallback = async (accessToken, refreshToken, profile, cb) => {
+	const { _json: {id, name, email} } = profile;
+	try {
+		const user = await User.findOne({email});
+		if(user) {
+			user.facebookId = id;
+			user.avatarUrl = `https://graph.facebook.com/${id}/picture?type=large`;
+			user.save();
+			return cb(null, user);
+		}
+		const newUser = await User.create({
+			email,
+			name,
+			facebookId: id,
+			avatarUrl: `https://graph.facebook.com/${id}/picture?type=large`
+			
+		})
+	} catch(error) {
+		return cb(error);
+	}
+}
+
+export const postFacebookLogin = (req, res) => {
+	res.redirect(routes.home);
+};
+
 export const logout = (req, res) => {
-  // To Do: Process Log Out
+  req.logout();
   res.redirect(routes.home);
 };
 
-export const userDetail = (req, res) =>
-  res.render("userDetail", { pageTitle: "User Detail" });
+export const getme = (req, res) => {
+	res.render("userDetail", { pageTitle: "User Detail", user: req.user });
+};
+
+export const userDetail = async (req, res) => {
+	const { params: { id } } = req;
+	try {
+		const user = await User.findById(id);
+	    res.render("userDetail", { pageTitle: "User Detail", user });
+	} catch(error) {
+		res.redirect(routes.home);
+}
+};
+
 export const editProfile = (req, res) =>
   res.render("editProfile", { pageTitle: "Edit Profile" });
 export const changePassword = (req, res) =>
